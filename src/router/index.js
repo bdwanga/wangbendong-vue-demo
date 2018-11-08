a0793dc3472a78208882988f40f3ee59d7e7fb45
wbd <1401710845@qq.com>
2018-11-08 16:54:18 +0800

    TASK:vue用户管理 搭架子

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,37 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import manger from '@/views/frame/manger'
+import home from '@/views/frame/home'
+import userList from '@/views/user/userList'
+import orgTree from '@/views/org/orgTree'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/hello',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/',
+      component: manger,
+      name: '',
+      children: [{
+        path: '',
+        component: home,
+        meta: []
+      }, {
+        path: '/userList',
+        component: userList,
+        meta: ['数据管理', '用户管理']
+      }, {
+        path: '/orgTree',
+        component: orgTree,
+        meta: ['数据管理', '组织管理']
+      }]
     }
   ]
 })
--- src/views/frame/manger.vue
+++ src/views/frame/manger.vue
@@ -31,7 +31,7 @@ export default {
 </script>
 
 <style lang="less" scoped>
-  @import '../../style/mixin';
+  @import '../style/mixin';
   .manage_page{
 
   }
