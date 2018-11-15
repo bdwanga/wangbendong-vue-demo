fa65fd29a3a587b6e069dae0dd5889e6caf49f60
wbd <1401710845@qq.com>
2018-11-15 17:51:44 +0800

    TASK:vue用户管理 修改配置

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://localhost:12251/'
+        target: 'http://localhost:22251/'
       }
     },
 
@@ -54,7 +54,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
