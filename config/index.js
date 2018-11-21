f8265e1936966f5b90eeabd97e496f3433314fc0
wbd <1401710845@qq.com>
2018-11-21 11:45:52 +0800

    UPDATE:vue用户管理 优化代码，增加zip打包方式

--- config/index.js
+++ config/index.js
@@ -71,6 +71,8 @@ module.exports = {
     productionGzip: false,
     productionGzipExtensions: ['js', 'css'],
 
+    productionZip:true,
+
     // Run the build command with an extra argument to
     // View the bundle analyzer report after build finishes:
     // `npm run build --report`
